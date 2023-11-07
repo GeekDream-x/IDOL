70efc1be3ff4c3942ff0f23e2b25962a1a797d19
GeekDream-x <inace0302@gmail.com>
2023-11-07 17:28:12 +0800

    update debertav2formultiplechoices

--- transformers/__init__.py
+++ transformers/__init__.py
@@ -718,6 +718,7 @@ if is_torch_available():
         [
             "DEBERTA_V2_PRETRAINED_MODEL_ARCHIVE_LIST",
             "DebertaV2ForMaskedLM",
+            "DebertaV2ForMultipleChoice",
             "DebertaV2ForQuestionAnswering",
             "DebertaV2ForSequenceClassification",
             "DebertaV2ForTokenClassification",
@@ -2397,6 +2398,7 @@ if TYPE_CHECKING:
         from .models.deberta_v2 import (
             DEBERTA_V2_PRETRAINED_MODEL_ARCHIVE_LIST,
             DebertaV2ForMaskedLM,
+            DebertaV2ForMultipleChoice,
             DebertaV2ForQuestionAnswering,
             DebertaV2ForSequenceClassification,
             DebertaV2ForTokenClassification,
